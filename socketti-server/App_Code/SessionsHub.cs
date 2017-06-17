15bd11be8d7d8407987db9e14e4452455e232f09
Dacre <enquiries@mooce.co.nz>
2017-06-17 21:10:41 +1200

    first commit
    Signed-off-by: Dacre <enquiries@mooce.co.nz>

---
+++ socketti-server/App_Code/SessionsHub.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace two_way_sockets.App_Code
+{
+    public class SessionsHub : Hub
+    {
+        public Task AddGroups()
+        {
+            return Groups.Add(Context.ConnectionId, "english");
+        }
+
+        public void SendMessage(string message)
+        {
+            Clients.Group("english").SendMessage("ENGLISH:" + message);
+            // Call the JavaScript method sendSessionChanged on all the clients
+            Clients.All.SendMessage(message);
+        }
+
+        ////automatically join groups when client first connects
+        //public override Task OnConnected()
+        //{
+        //    return AddGroups();
+        //}
+
+        ////rejoin groups if client disconnects and then reconnects
+        //public override Task OnReconnected()
+        //{
+        //    return AddGroups();
+        //}
+    }
+}
